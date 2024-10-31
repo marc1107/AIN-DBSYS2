b2a3b64c2c4add7a9a3099a574da7efd419c1365
marc1107 <m.bohner99@gmail.com>
2024-10-31 11:45:09 +0100

    Finished Blatt 2

--- Uebungsblatt2/Aufgabe1.sql
+++ Uebungsblatt2/Aufgabe1.sql
@@ -2,7 +2,7 @@ ALTER TABLE pers
 ADD accountname VARCHAR(255);
 
 UPDATE pers
-SET accountname = 'DBSYS96'
+SET accountname = 'DBSYS120'
 WHERE pnr = 123;
 
 
@@ -18,18 +18,12 @@ FROM pers
 WHERE vnr = (SELECT pnr FROM pers WHERE accountname = USER);
 
 
-GRANT SELECT ON myself TO dbsys96;
-GRANT SELECT ON mypers TO dbsys96;
+GRANT SELECT ON myself TO PUBLIC;
+GRANT SELECT ON mypers TO PUBLIC;
 
 
-GRANT UPDATE (name, jahrg, eindat, beruf) ON myself TO dbsys96;
-GRANT UPDATE (gehalt, anr, vnr) ON mypers TO dbsys96;
-
-
--- Error wenn Revoke auf den eigenen User ausgeführt wird???
-REVOKE UPDATE ON mypers FROM dbsys96;
-REVOKE UPDATE ON myself FROM dbsys96;
-REVOKE SELECT ON myself FROM dbsys96;
+GRANT UPDATE (name, jahrg, eindat, beruf) ON myself TO PUBLIC;
+GRANT UPDATE (gehalt, anr, vnr) ON mypers TO PUBLIC;
 
 
 SELECT * FROM myself;
@@ -37,7 +31,8 @@ SELECT * FROM mypers;
 
 
 -- Warum funktioniert das immer noch? --> Vermutlich weil Revoke command nicht richtig ausgeführt wurde
---UPDATE myself SET gehalt = gehalt + 1000 WHERE pnr = 123;
+UPDATE myself SET gehalt = gehalt + 1000 WHERE pnr = 123;
+
 
 
 COMMIT;
---
+++ Uebungsblatt2/Aufgabe1_Test.sql
@@ -0,0 +1,4 @@
+UPDATE dbsys96.myself SET name = 'maier';
+
+select * from dbsys96.myself;
+select * FROM DBSYS96.MYPERS;
